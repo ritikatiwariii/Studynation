8ef4873c3bbc69aacc7a145cfed7c83172cfd47c
Ritika Tiwari <166583127+ritikatiwariii@users.noreply.github.com>
2025-08-17 17:38:13 +0530

    Update courseEnrollmentEmail.js

--- server/mail/templates/courseEnrollmentEmail.js
+++ server/mail/templates/courseEnrollmentEmail.js
@@ -78,7 +78,7 @@ exports.courseEnrollmentEmail = (courseName, name) => {
                     are excited to have you as a participant!</p>
                 <p>Please log in to your learning dashboard to access the course materials and start your learning journey.
                 </p>
-                <a class="cta" href="https://studynation-frontend2.onrender.com/dashboard">Go to Dashboard</a>
+                <a class="cta" href="https://studynation-frontend2.onrender.com/dashboard/enrolled-courses">Go to Dashboard</a>
             </div>
             <div class="support">If you have any questions or need assistance, please feel free to reach out to us at <a
                     href="mailto:info@studynotion.com">info@studynotion.com</a>. We are here to help!</div>
